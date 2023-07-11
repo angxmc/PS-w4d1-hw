73b9dab936a35f87e7ebfedbee453b16a283dc97
Your Name <you@example.com>
2023-07-11 15:13:24 -0400

    created timmys story

--- w4d1hw_classes.js
+++ w4d1hw_classes.js
@@ -65,8 +65,8 @@ class Person {
     this.mood--;
     this.bankAccount+10;
   }
-  buyHamster(){
-    const hamster = new Hamster();
+  buyHamster(hamsterName){
+    const hamster = new Hamster(this.name, hamsterName);
     this.hamsters.push(hamster);
     this.mood++;
     this.bankAccount--;
@@ -76,3 +76,48 @@ class Person {
 const person1 = new Person();
 person1.buyHamster();
 console.log(person1);
+
+console.log("Create a story with your Person class");
+// ** Create a story with your Person class -----------------------------------------------
+//Instantiate a new Person named Timmy
+const timmy = new Person ("Timmy")
+//Age Timmy five years
+console.log(timmy);
+for (let i = 0; i < 5; i++) {
+  timmy.ageUp(); 
+}
+console.log(timmy);
+//At this point Timmy's a little bummed. As a precocious child, he feels he's "seen it all" already. Have him eat five times.
+for (let i = 0; i < 5; i++) {
+  timmy.eat();
+}
+console.log(timmy);
+//Now Timmy's a little heavier than he wants to be. Kindergarten's coming up and he wants to look good. Have him exercise five times\
+for (let i = 0; i < 5; i++) {
+  timmy.exercise();
+}
+console.log(timmy);
+//Age Timmy 9 years
+for (let i = 0; i < 9; i++) {
+  timmy.ageUp();
+}
+//Create a hamster named "Gus",Set Gus's owner to the string "Timmy"
+const hamsterGus = new Hamster('Timmy',"Gus");
+console.log(hamsterGus);
+// ? Have Timmy "buy" Gus
+timmy.buyHamster(hamsterGus);
+console.log(timmy);
+//Age Timmy 15 years
+for (let i = 0; i < 15; i++) {
+  timmy.ageUp();
+}
+console.log(timmy);
+//Have Timmy eat twice
+for (let i = 0; i < 2; i++) {
+  timmy.eat();
+}
+console.log(timmy);
+//Have Timmy exercise twice
+for (let i = 0; i < 2; i++) {
+  timmy.exercise();
+}
